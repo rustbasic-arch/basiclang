d830b6a3a46346a8b33cb42b5a34a4593d6ea496
wenweiping <wenweiping@huayi.com>
2019-10-30 11:22:58 +0800

    first oop in rust

--- RunHello/src/main.rs
+++ RunHello/src/main.rs
@@ -1,3 +1,34 @@
+
+
 fn main() {
-    println!("Hello, world!");
+    #[derive(Debug)]
+     struct Msg{
+         id:i64,
+         name:String,
+         password:String
+     }
+
+     impl Msg {
+         fn new(id:i64,name:String,password:String)->Self{
+            Msg{
+                name,
+                id,
+                password
+            }
+         }
+         fn setName(&mut self,n:String){
+             self.name = n;
+         }
+         fn print(&self){
+             println!("{:#?}",self);
+         }
+     }
+
+   
+     let mut m = Msg::new(123 as i64, String::from("wenwp"),String::from("123456"));
+     m.print();
+     m.setName(String::from("newton"));
+     m.print();
+    
+
 }
---
+++ RunHello/target/debug/RunHello.d
@@ -0,0 +1,1 @@
+E:\workspace\fengyun\RunHello\target\debug\RunHello.exe: E:\workspace\fengyun\RunHello\src\main.rs
