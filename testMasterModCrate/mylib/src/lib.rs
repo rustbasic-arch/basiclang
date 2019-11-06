83624c022b3ebdb7685b46b1a7ba98295d9367ca
wenweiping <wenweiping@huayi.com>
2019-11-06 15:14:09 +0800

    commit lib 注释

--- testMasterModCrate/mylib/src/lib.rs
+++ testMasterModCrate/mylib/src/lib.rs
@@ -1,4 +1,4 @@
 
 pub mod authmod;//only declare 
-use authmod::authImpl;//so realy import here to export 
+use authmod::authImpl::createAuth;//so realy import here to export (release api symbol here ,越详细越精细控制开放)
 pub mod friendmod;
